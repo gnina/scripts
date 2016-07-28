4007dccb52dc731ec29ada69e25b1258d9099026
Elisa Idrobo <idroboe1@tcnj.edu>
2016-07-28 17:02:14 -0400

    add max ligand pose and option to read in predictions from file

---
+++ bootstrap.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+import predict
+import sklearn.metrics
+import argparse, sys
+import os
+import numpy as np
+import glob
+import re
+import matplotlib.pyplot as plt
+
+def calc_auc(predictions):
+	y_true =[]
+	y_score=[]
+	for line in predictions:
+		values= line.split(" ")
+		y_true.append(float(values[1]))
+		y_score.append(float(values[0]))
+	auc = sklearn.metrics.roc_auc_score(y_true,y_score)
+	return auc
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='bootstrap(sampling with replacement) test')
+	parser.add_argument('-m','--model',type=str,required=True,help="Model template. Must use TESTFILE with unshuffled, unbalanced input")
+	parser.add_argument('-w','--weights',type=str,required=True,help="Model weights (.caffemodel)")
+	parser.add_argument('-i','--input',type=str,required=True,help="Input .types file to predict")
+	parser.add_argument('-g','--gpu',type=int,help='Specify GPU to run on',default=-1)
+	parser.add_argument('-o','--output',type=str,default='',help='Output file name,default= predict_[model]_[input]')
+	parser.add_argument('--iterations',type=int,default=1000,help="number of times to bootstrap")
+	parser.add_argument('-k','--keep',action='store_true',default=False,help="Don't delete prototxt files")
+	parser.add_argument('-n', '--number',action='store_true',default=False,help="if true uses caffemodel/input as is. if false uses all folds")
+	parser.add_argument('--max_score',action='store_true',default=False,help="take max score per ligand as its score")
+	parser.add_argument('--notcalc_predictions', type=str, default='',help='file of predictions')
+	args = parser.parse_args()
+	if args.output == '':
+		output = 'bootstrap_%s_%s'%(args.model, args.input)
+	else:
+		output = args.output
+	outname=output	
+	predictions=[]
+	if args.notcalc_predictions=='':
+		cm = args.weights
+		ts = args.input
+		if not args.number:
+			foldnum = re.search('.[0-9]_iter',cm).group()
+			cm=cm.replace(foldnum, '.[0-9]_iter')
+			foldnum = re.search('[0-9].types',ts).group()
+			ts=ts.replace(foldnum, '[NUMBER].types')
+		
+		for caffemodel in glob.glob(cm):
+			testset = ts
+			if not args.number:
+				num = re.search('.[0-9]_iter',caffemodel).group()
+				num=re.search(r'\d+', num).group()
+				testset = ts.replace('[NUMBER]',num)
+			args.input = testset
+			args.weights = caffemodel
+			predictions.extend(predict.predict(args))
+	elif args.notcalc_predictions != '':
+		for line in open(args.notcalc_predictions).readlines():
+			predictions.append(line)
+		
+	all_aucs=[]
+	for _ in xrange(args.iterations):
+		sample = np.random.choice(predictions,len(predictions), replace=True)
+		all_aucs.append(calc_auc(sample))
+	mean=np.mean(all_aucs)
+	std_dev = np.std(all_aucs)
+	txt = 'mean: %.2f standard deviation: %.2f'%(mean,std_dev)
+	print txt
+	output = open(output, 'w')
+	output.writelines('%.2f\n' %auc for auc in all_aucs)
+	output.write(txt)
+	output.close()
+
+	plt.figure()
+	plt.boxplot(all_aucs,0,'rs',0)
+	plt.title('%s AUCs'%args.output, fontsize=22)
+	plt.xlabel('AUC(%s)'%txt, fontsize=18)
+	plt.savefig('%s_plot.pdf'%outname,bbox_inches='tight')
+
+
+
+	
--- predict.py
+++ predict.py
@@ -8,45 +8,77 @@ import glob, re, sklearn, collections, argparse, sys, os
 import sklearn.metrics
 import caffe
 
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Train neural net on binmap data.')
-    parser.add_argument('-m','--model',type=str,required=True,help="Model template. Must use TESTFILE with unshuffled, unbalanced input")
-    parser.add_argument('-w','--weights',type=str,required=True,help="Model weights (.caffemodel)")
-    parser.add_argument('-i','--input',type=str,required=True,help="Input binmaps to predict")
-    parser.add_argument('-g','--gpu',type=int,help='Specify GPU to run on',default=-1)
-    parser.add_argument('-o','--output',type=str,help='Output file name',default='-')
-    parser.add_argument('-k','--keep',action='store_true',default=False,help="Don't delete prototxt files")
-    
-    args = parser.parse_args()
-    
+def predict(args):
     if args.gpu >= 0:
         caffe.set_device(args.gpu)
     caffe.set_mode_gpu()
-    
     model = open(args.model).read().replace('TESTFILE',args.input)
     #very obnoxiously, python interface requires network definition to be in a file
     testfile = 'predict.%d.prototxt' % os.getpid()
     with open(testfile,'w') as f:
         f.write(model)    
-
     net = caffe.Net(testfile, args.weights, caffe.TEST)
-        
-    if args.output == '-':
-        out = sys.stdout
-    else:
-        out = open(args.output,'w')
-        
+    output =[]
     for line in open(args.input):
         predict = net.forward()['output']
         if predict.shape[0] != 1:
             print "Error: require single-sized batches"
             sys.exit(1)
         
-        out.write('%f %s' % (predict[0][1],line))
-    
+        output.append('%f %s' % (predict[0][1],line))
+    if args.max_score: output = maxLigandScore(output)
     if not args.keep:
         os.remove(testfile)
+    return output
+
+def maxLigandScore(output):
+	#output format: score label paths 
+	ligands ={}
+	for line in output:
+		data = line.split(" ",2)
+		score = data[0]
+		true_class = data[1]
+		path = data [2] # assumes that it contains [folder]/[ligname]_[#].gninatypes
+		targetname= path.split("/",1)[0]
+		ligname = path.rsplit("/",1)[-1]
+		ligname = ligname.split(".gninatypes")[0]
+		ligname = ligname.split("_")[0]
+		key = targetname+ligname
+		if key in ligands:
+			if ligands[key][0] < score: ligands[key]=(score, '%s %s'%(true_class,path))
+		else: ligands[key]=(score, '%s %s'%(true_class,path))
+	new_output =ligands.values()
+	for i in xrange(len(new_output)):
+		new_output[i]= '%s %s'%(new_output[i][0],new_output[i][1])
+	return new_output
+	
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Train neural net on binmap data.')
+    parser.add_argument('-m','--model',type=str,required=True,help="Model template. Must use TESTFILE with unshuffled, unbalanced input. EX: file.model ")
+    parser.add_argument('-w','--weights',type=str,required=True,help="Model weights (.caffemodel)")
+    parser.add_argument('-i','--input',type=str,required=True,help="Input .types file to predict")
+    parser.add_argument('-g','--gpu',type=int,help='Specify GPU to run on',default=-1)
+    parser.add_argument('-o','--output',type=str,help='Output file name',default='-')
+    parser.add_argument('-k','--keep',action='store_true',default=False,help="Don't delete prototxt files")
+    parser.add_argument('--max_score',action='store_true',default=False,help="take max score per ligand as its score")
+    parser.add_argument('--notcalc_predictions', type=str, default='',help='file of predictions')
+
+    args = parser.parse_args()
+    if args.output == '-':
+        out = sys.stdout
+    else:
+        out = open(args.output,'w')
+    if args.notcalc_predictions == '': output = predict(args)
+    else:
+		predictions=[]
+		for line in open(args.notcalc_predictions).readlines():
+			predictions.append(line)
+		if args.max_score: output=maxLigandScore(predictions)
+    out.writelines('%s'%line for line in output)
+    
+    
+    
 
     
     
