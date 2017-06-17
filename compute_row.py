2128254cee2c90996e967a12f2fa6e0fbe491a3f
David Koes <dkoes@pitt.edu>
2017-06-17 14:27:22 -0400

    bugfixes

--- compute_row.py
+++ compute_row.py
@@ -3,7 +3,7 @@
 '''Compute a single row of a distance matrix from a pdbinfo file.  
 This allows for distributed processing'''
 
-import clustering
+import clustering,argparse,sys
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Compute a single row of a distance matrix from a pdbinfo file.')
@@ -11,19 +11,19 @@ if __name__ == '__main__':
     parser.add_argument('-r','--row',type=int,required=True,help="row to compute")
     parser.add_argument('--out',help='output file (default stdout)',type=argparse.FileType('w'),default=sys.stdout)
 
+    
     args = parser.parse_args()
     
-    target_names, targets = train.readPDBfiles(args.pdbfiles)
-
+    target_names, targets = clustering.readPDBfiles(args.pdbfiles)
     r = args.row
     if r < len(target_names):
         name = target_names[r]
         row = []
-        for i in xrange(len(target_names):
-            (a, b, mindist) = train.cUTDM2(targets, (r,i))
+        for i in xrange(len(target_names)):
+            (a, b, mindist) = clustering.cUTDM2(targets, (r,i))
             row.append((target_names[i],mindist))
         #output somewhat verbosely
         for (n,dist) in row:
-            args.out.write('%s %s %f\n'%(name,n,dist)
+            args.out.write('%s %s %f\n'%(name,n,dist))
     else:
         print "Invalid row",r,"with only",len(target_names),"targets"
