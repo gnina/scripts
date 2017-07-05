5526a27dd617387c678aa89f94077a049a04d2ca
David Koes <dkoes@pitt.edu>
2017-07-05 17:25:37 -0400

    path

--- affinity_search/evaluate.py
+++ affinity_search/evaluate.py
@@ -11,6 +11,7 @@ import numpy as np
 import os, sys
 os.environ["GLOG_minloglevel"] = "10"
 sys.path.append("/home/dkoes/git/gninascripts/")
+sys.path.append("/net/pulsar/home/koes/dkoes/git/gninascripts/")
 
 import train, predict
 import matplotlib, caffe
