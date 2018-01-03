4813edd701ab011916494c262c290b96e9cf70c6
David Koes <dkoes@pitt.edu>
2018-01-03 16:31:59 -0500

    dateroot for timemodel

--- timemodel.py
+++ timemodel.py
@@ -7,12 +7,14 @@ parser = argparse.ArgumentParser(description='Train neural net on .types data.')
 parser.add_argument('-m','--model',type=str,required=True,help="Model template. Must use TRAINFILE and TESTFILE")
 parser.add_argument('-p','--prefix',type=str,required=True,help="Prefix for training/test files: <prefix>[train|test][num].types")
 parser.add_argument('-o','--output',type=str,help="Output model (default timeit.model)",default="timeit.model")
+parser.add_argument('--data_root',type=str,help="Root directory for gninatypes files",default="..")
 
 args = parser.parse_args()
 
 model = open(args.model).read()
 model = model.replace('TRAINFILE','%strain0.types'%args.prefix)
 model = model.replace('TESTFILE','%stest0.types'%args.prefix)
+model = model.replace('DATA_ROOT',args.data_root)
 
 out = open(args.output,'w')
 out.write(model)
