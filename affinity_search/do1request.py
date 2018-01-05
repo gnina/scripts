10d9b05c215d540797337d9e0c52941ea624c6fc
David Koes <dkoes@pitt.edu>
2018-01-04 23:57:04 -0500

    another bug fix

--- affinity_search/do1request.py
+++ affinity_search/do1request.py
@@ -95,6 +95,8 @@ config['auc'] = float(auc)
 config['id'] = d
 config['msg'] = 'Sucess'
 
-sql = 'UPDATE params SET {}'.format(', '.join('{}=%s'.format(k) for k in config))
+serial = config['serial']
+del config['serial']
+sql = 'UPDATE params SET {} WHERE serial = %s'.format(', '.join('{}=%s'.format(k) for k in config),serial)
 cursor = getcursor()
 cursor.execute(sql, config.values())
