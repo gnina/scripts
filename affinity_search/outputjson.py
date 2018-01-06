e6a9e4610fe108892ae8e096efcc9e019a70cad1
David Koes <dkoes@pitt.edu>
2018-01-06 14:10:02 -0500

    use spearmint to update request list

--- affinity_search/outputjson.py
+++ affinity_search/outputjson.py
@@ -9,23 +9,29 @@ import json, sys
 from collections import OrderedDict
 
 #extract from arguments to makemodel
-opts = makemodel.getoptions()
 
-d=OrderedDict()
-for (name,vals) in sorted(opts.items()):
-	paramsize=1
-	if type(vals) == tuple:
-		options=map(str,vals)
-		paramtype="enum"
-		data=OrderedDict([("name",name), ("type", paramtype), ("size", paramsize),("options",options)])
-	elif isinstance(vals, makemodel.Range):
-		parammin = vals.min
-		parammax = vals.max
-		paramtype="float"
-		data=OrderedDict([("name",name), ("type", paramtype), ("min", parammin), ("max", parammax), ("size", paramsize)])
-	else:
-		print "Unknown type"
-		sys.exit(-1)
-	d[name]=data
-sys.stdout.write(json.dumps(d, indent=4)+'\n')
+def makejson():
+	'''return dictionary of config options from makemodel'''
+	opts = makemodel.getoptions()
+
+	d=OrderedDict()
+	for (name,vals) in sorted(opts.items()):
+		paramsize=1
+		if type(vals) == tuple:
+			options=map(str,vals)
+			paramtype="enum"
+			data=OrderedDict([("name",name), ("type", paramtype), ("size", paramsize),("options",options)])
+		elif isinstance(vals, makemodel.Range):
+			parammin = vals.min
+			parammax = vals.max
+			paramtype="float"
+			data=OrderedDict([("name",name), ("type", paramtype), ("min", parammin), ("max", parammax), ("size", paramsize)])
+		else:
+			print "Unknown type"
+			sys.exit(-1)
+		d[name]=data
+	return d
+	
+if __name__ == '__main__':
+	sys.stdout.write(json.dumps(makejson(), indent=4)+'\n')
 
--- affinity_search/populaterequests.py
+++ affinity_search/populaterequests.py
@@ -10,30 +10,34 @@ import numpy as np
 
 opts = makemodel.getoptions()
 
+def addrows(fname,host,db,password,start=0):
+    '''add rows from fname into database, starting at row start'''
+    data = pd.read_csv(fname,delim_whitespace=True,header=None)
+    colnames = ['P1','P2']
+    for (name,val) in sorted(opts.items()):
+        colnames.append(name)
+        
+    data.columns = colnames
+    data = data.drop(['P1','P2'],axis=1)
 
-data = pd.read_csv(sys.argv[1],delim_whitespace=True,header=None)
-colnames = ['P1','P2']
-for (name,val) in sorted(opts.items()):
-    colnames.append(name)
-    
-data.columns = colnames
-data = data.drop(['P1','P2'],axis=1)
-
-conn = MySQLdb.connect (host = "35.196.158.205",user = "opter",passwd=sys.argv[2],db="opt1")
-cursor = conn.cursor()
+    conn = MySQLdb.connect (host = host,user = "opter",passwd=password,db=db)
+    cursor = conn.cursor()
 
 
-for (i,row) in data.iterrows():
-    names = ','.join(row.index)
-    values = ','.join(['%s']*len(row))
-    names += ',id'
-    values += ',"REQUESTED"'
-    #do three variations
-    for _ in xrange(3):
-        split = np.random.randint(0,5)
-        seed = np.random.randint(0,100000)
-        n = names + ',split,seed'
-        v = values + ',%d,%d' % (split,seed) 
-        insert = 'INSERT INTO params (%s) VALUES (%s)' % (n,v)
-        cursor.execute(insert,row)
-conn.commit()
+    for (i,row) in data[start:].iterrows():
+        names = ','.join(row.index)
+        values = ','.join(['%s']*len(row))
+        names += ',id'
+        values += ',"REQUESTED"'
+        #do three variations
+        for _ in xrange(3):
+            split = np.random.randint(0,5)
+            seed = np.random.randint(0,100000)
+            n = names + ',split,seed'
+            v = values + ',%d,%d' % (split,seed) 
+            insert = 'INSERT INTO params (%s) VALUES (%s)' % (n,v)
+            cursor.execute(insert,row)
+    conn.commit()
+
+if __name__ == '__main__':
+    addrows(sys.argv[1],"35.196.158.205","opt1",sys.argv[2])
