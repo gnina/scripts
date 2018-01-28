92c0c7f9f1ef86efb70131017fe7bf45abcf71d0
David Koes <dkoes@pitt.edu>
2018-01-28 15:00:11 -0500

    bugfix

--- affinity_search/fix.py
+++ affinity_search/fix.py
@@ -37,8 +37,8 @@ for row in rows:
         
     print row['id']
 
-    cmdline = './reval.py --prefix %s --data_root "%s" --split %d --dir %s' % \
-            (args.prefix,args.data_root,row['split'], row['id'])
+    cmdline = '%s --prefix %s --data_root "%s" --split %d --dir %s' % \
+            (args.reval, args.prefix,args.data_root,row['split'], row['id'])
     print cmdline
     
     #call runline to insulate ourselves from catestrophic failure (caffe)
