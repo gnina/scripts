c2a16afcd0bab2900b9c21d52d0d9d8c0b4afdc6
David Koes <dkoes@pitt.edu>
2018-04-12 17:07:59 -0400

    some bandaid fixes for predict (really needs a refactoring)

--- predict.py
+++ predict.py
@@ -6,6 +6,7 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import glob, re, sklearn, collections, argparse, sys, os
 import sklearn.metrics
+import scipy
 import caffe
 from caffe.proto.caffe_pb2 import NetParameter
 import google.protobuf.text_format as prototxt
@@ -41,6 +42,7 @@ def predict(args):
     y_score = result.y_score
     loss = result.loss
     rmsd = result.rmsd
+    pearsonr = None
     y_affinity = result.y_aff
     y_predaff = result.y_predaff
     
@@ -63,29 +65,24 @@ def predict(args):
         output_lines = [t for t in zip(y_score, lines)]
                         
 
+    #this is all awkward and should be rewritten with a smarter approach than munging strings
     if args.max_score or args.max_affinity:
         output_lines = maxLigandScore(output_lines, args.max_affinity)
-        #have to recalculate RMSD and AUC
-        labelindex = 1
-        if rmsd != None: labelindex = 2
-        affpredindex = 0
-        afflabelindex = 2
-        if auc != None: 
-            affpredindex = 1
-            afflabelindex = 3
+        #have to recalculate RMSD and AUC  
             
         if auc != None:
-            y_true = [float(line.split()[labelindex]) for line in output_lines]
-            y_score = [float(line.split()[0]) for line in output_lines]
+            y_true = [float(line[-1].split()[0]) for line in output_lines]
+            y_score = [line[0] for line in output_lines]
             auc = sklearn.metrics.roc_auc_score(y_true, y_score)
         if rmsd != None:
-            y_affinity = [float(line.split()[afflabelindex]) for line in output_lines]
-            y_predaff = [float(line.split()[affpredindex]) for line in output_lines]
+            y_affinity = [float(line[-1].split()[1]) for line in output_lines]
+            y_predaff = [line[1] for line in output_lines]
             rmsd = np.sqrt(sklearn.metrics.mean_squared_error(np.abs(y_affinity),y_predaff))
-        
+            pearsonr = scipy.stats.pearsonr(np.abs(y_affinity),y_predaff)[0]
+
     if not args.keep:
         os.remove(test_model)
-    return output_lines,auc,rmsd
+    return output_lines,auc,rmsd,pearsonr
 
 def predict_lines(args):
     '''Return previous format of a list of strings corresponding to the output lines of a prediction file'''
@@ -95,12 +92,14 @@ def predict_lines(args):
         l = ''
         for val in line[:-1]:
             l += '%f '%val
-        l += '%s\n' % line[-1]
+        l += '| %s' % line[-1]
         lines.append(l)
     if predictions[1] != None:
         lines.append('# AUC %f\n'%predictions[1])
     if predictions[2] != None:
         lines.append('# rmsd %f\n'%predictions[2])
+    if predictions[3] != None:
+        lines.append('# pearsonr %f\n'%predictions[3])
     return lines
 
 def get_ligand_key(rec_path, pose_path):
@@ -123,7 +122,8 @@ def maxLigandScore(lines, useaff):
     #output format: score label [affinity] rec_path pose_path
     ligands = {}
     for line in lines:
-        data = line.split('#')[0].split()
+        data = line[2].split('#')[0].split()
+        data = list(line[:2])+data
         if len(data) == 4: #only score present
             score = float(data[0])
             rec_path = data[2].strip()
