3ad52e87e0fc3a141bb281e052b40012cee842cf
David Koes <dkoes@pitt.edu>
2018-05-29 02:10:26 +0000

    msg inprogressname

--- affinity_search/do1request.py
+++ affinity_search/do1request.py
@@ -82,7 +82,7 @@ else:
     config = None
     for row in rows:
         # need to atomically update id
-        ret = cursor.execute('UPDATE params SET id = "INPROGRESS" WHERE serial = %s AND id = "REQUESTED"',[row['serial']])
+        ret = cursor.execute('UPDATE params SET id = "INPROGRESS", msg = "%s" WHERE serial = %s AND id = "REQUESTED"',[inprogressname,row['serial']])
         if ret: # success!
             #set config
             config = row
